d7d3e1f51aa13ff8a842a843d7ff5080620877ec
EmptyCharacter <farin.k.joshua@gmail.com>
2022-08-14 14:40:37 -1000

    Add banks class for static reads

---
+++ e-PluginUtils/src/main/java/dev/empty/pluginutils/bank/Banks.java
@@ -0,0 +1,8 @@
+package dev.empty.pluginutils.bank;
+
+import net.runelite.api.coords.WorldArea;
+
+public class Banks
+{
+    public static final WorldArea GrandExchangeBank = new WorldArea(3164, 3487, 1, 1, 0);
+}
