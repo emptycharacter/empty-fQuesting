b9af7194903fe59d413d5bf584bfa8e4db0b0ff9
EmptyCharacter <farin.k.joshua@gmail.com>
2022-08-14 15:04:24 -1000

    Update Banks:
    not sure if this is necessary with pathfinder api

--- e-PluginUtils/src/main/java/dev/empty/pluginutils/bank/Banks.java
+++ e-PluginUtils/src/main/java/dev/empty/pluginutils/bank/Banks.java
@@ -5,4 +5,8 @@ import net.runelite.api.coords.WorldArea;
 public class Banks
 {
     public static final WorldArea GrandExchangeBank = new WorldArea(3164, 3487, 1, 1, 0);
+    public static final WorldArea LumbridgeBank = new WorldArea(3208, 3220, 1, 1, 0);
+    public static final WorldArea DraynorBank = new WorldArea(3092, 3200, 1, 1, 0);
+    public static final WorldArea FaladorBank = new WorldArea(2946, 3368, 1, 1, 0);
+//    public static final WorldArea EdgevilleBank = new WorldArea();
 }
