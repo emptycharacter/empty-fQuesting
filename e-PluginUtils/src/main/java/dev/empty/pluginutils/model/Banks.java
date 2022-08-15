685592f37fa03d4cce4cfc944691bb9de83353a3
EmptyCharacter <farin.k.joshua@gmail.com>
2022-08-15 09:00:44 -1000

    checkstyle

--- e-PluginUtils/src/main/java/dev/empty/pluginutils/model/Banks.java
+++ e-PluginUtils/src/main/java/dev/empty/pluginutils/model/Banks.java
@@ -8,5 +8,6 @@ public class Banks
     public static final WorldArea LumbridgeBank = new WorldArea(3208, 3220, 1, 1, 0);
     public static final WorldArea DraynorBank = new WorldArea(3092, 3200, 1, 1, 0);
     public static final WorldArea FaladorBank = new WorldArea(2946, 3368, 1, 1, 0);
-//    public static final WorldArea EdgevilleBank = new WorldArea();
 }
+
+
