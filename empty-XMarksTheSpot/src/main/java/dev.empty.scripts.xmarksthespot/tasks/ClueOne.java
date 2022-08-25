71444de64bfb2e2f02bbd1afda99b4216a5ddd2c
EmptyCharacter <farin.k.joshua@gmail.com>
2022-08-25 02:34:35 -1000

    scaffold xmarks

---
+++ empty-XMarksTheSpot/src/main/java/dev.empty.scripts.xmarksthespot/tasks/ClueOne.java
@@ -0,0 +1,16 @@
+package dev.empty.scripts.xmarksthespot.tasks;
+
+public class ClueOne implements ScriptTask
+{
+    @Override
+    public boolean validate()
+    {
+        return false;
+    }
+
+    @Override
+    public int execute()
+    {
+        return 0;
+    }
+}
